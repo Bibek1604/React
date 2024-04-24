bdfc5d293711e4866e5a7b024a8c950324c67a99
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-24 23:05:56 +0545

    peopes and tailwind added

---
+++ part1/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Card  from './component/card'  
+
+function App() {
+
+  return (
+    <>
+
+    <h1 className='bg-green-400 p-4 rounded-xl '>Tailwind Test</h1>
+    <card></card>
+
+
+
+
+    </>
+
+  )
+}
+
+export default App
