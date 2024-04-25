857706e91a5b5833da9e92afb390e133c20ecfa5
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-25 18:28:27 +0545

    counter app crreatedd

--- counter project/src/App.jsx
+++ counter project/src/App.jsx
@@ -1,27 +1,35 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(){
   const [counter, setCounter] = useState(0);
 
 
-function addValue(){
- setCounter(counter+1) 
+function add(){
+  setCounter(counter+1)
 }
 
-  function removeValue() {
-    setCounter(counter - 1);
-  }
+function less(){
+  setCounter(counter-1)
+}
 
-  return (
-    <>  
-<h1>Counter:{counter}</h1>
-<button onClick={addValue}>add</button>
-<br />
-<br />
-<button onClick={removeValue}>Less</button>
-    </>
-  );
+function clear(){
+  setCounter(0)
+}
+
+return (
+
+  <>
+
+    <h1>COunter App</h1>
+    <p>counter:{counter}</p>
+    <button onClick={add}>Add</button>
+
+    <button onClick={less}>Less</button>
+
+    <button onClick={clear}> Clear</button>
+  </>
+);
 }
 
 export default App;
