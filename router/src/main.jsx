5facad77924ac2048cce35471f83f894be30fa1c
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-02 00:19:36 +0545

    changed

--- router/src/main.jsx
+++ router/src/main.jsx
@@ -16,8 +16,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
       {/* <Route path='user/:userid' element={<User />} /> */}
       {/* <Route  */}
       {/* // loader={githubInfoLoader}
