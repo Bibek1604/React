15127b472dc7e0ed7a4ba9a90fb7fd2f495a67bb
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-04 18:58:02 +0545

    all setup of react and tailwind

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -6,7 +6,14 @@ function App() {
 
   return (
     <>
-    <button className='bg-green-500 rounded-lg shadow-2xl		'>CLick me </button>
+    <div className='bg-amber-950	h-5/6	w-5/6'>
+    <button className='bg-green-500 rounded-lg shadow-2xl'>CLick me for betweewiwoirjgnljsnkl</button>
+    <button className='bg-green-500 rounded-lg shadow-2xl'>CLick me for betweewiwoirjgnljsnkl</button>
+    <button className='bg-green-500 rounded-lg shadow-2xl'>CLick me for betweewiwoirjgnljsnkl</button>
+    <button className='bg-green-500 rounded-lg shadow-2xl'>CLick me for betweewiwoirjgnljsnkl</button>
+
+    </div>
+    
     </>
   )
 }
