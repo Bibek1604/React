8f2adb8d740ddeea2457b2c763cf92f1757b00aa
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-04 22:38:59 +0545

    added cards

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,21 +1,23 @@
-
 import './App.css'
+import Card from "./components/Card";
 
-function App() {
-  
-
-  return (
+function app(){
+  return(
     <>
-    <div className='bg-amber-950	h-5/6	w-5/6'>
-    <button className='bg-green-500 rounded-lg shadow-2xl'>CLick me for betweewiwoirjgnljsnkl</button>
-    <button className='bg-green-500 rounded-lg shadow-2xl'>CLick me for betweewiwoirjgnljsnkl</button>
-    <button className='bg-green-500 rounded-lg shadow-2xl'>CLick me for betweewiwoirjgnljsnkl</button>
-    <button className='bg-green-500 rounded-lg shadow-2xl'>CLick me for betweewiwoirjgnljsnkl</button>
+    <Card username="bibekpandey" description="hello this is my word" buttonText="checkthis out"/>
+    <Card username="bibekpandey" description="hello this is my word" buttonText="checkthis out"/>
 
-    </div>
-    
-    </>
+    <Card username="bibekpandey" description="hello this is my word" buttonText="checkthis out"/>
+    <Card username="bibekpandey" description="hello this is my word"
+   buttonText="checkthis out"/>
+       <Card username="bibekpandey" description="hello this is my word" buttonText="checkthis out"/>
+
+       <Card username="bibekpandey" description="hello this is my word" buttonText="checkthis out"/>
+
+
+
+
+  </>
   )
 }
-
-export default App
+export default app
