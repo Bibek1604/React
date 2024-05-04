c872cc86c907ddfb4e95048c38d4d69771771973
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-04 18:51:52 +0545

    all setup of react and tailwind

---
+++ props/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <button className='bg-green-500 rounded-lg shadow-2xl		'>CLick me </button>
+    </>
+  )
+}
+
+export default App
