36f0fda85f6766152e7478b25954fc0c5beceb54
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-05 17:50:32 +0545

    bg-color

--- bgcolor/src/App.jsx
+++ bgcolor/src/App.jsx
@@ -1,16 +1,27 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [color,setColor] =useState('black')
+
 
   return (
-    <>
-    <div className='flex w-full bg-gray-400 min-h-24 items-center justify-between'>
 
-<div className='min-h-14 w-14 bg-green-300'>green</div>
-<div className='min-h-14 w-14 bg-blue-300'>blue</div>
-<div className='min-h-14 w-14 bg-yellow-300'>yellow</div>
-<div className='min-h-14 w-14 bg-red-300'>red</div>
+
+
+
+    <>
+    <div className='flex w-full bg-gray-400 min-h-24 items-center justify-between' style={{backgroundColor:color}}>
+
+<div onClick={()=>setColor("green")}  className='min-h-14 w-14 bg-green-300'>green</div>
+<div onClick={()=>setColor("blue")}className='min-h-14 w-14 bg-blue-300'>blue</div>
+<div onClick={()=>setColor("yellow")}className='min-h-14 w-14 bg-yellow-300'>yellow</div>
+<div onClick={()=>setColor("red")}className='min-h-14 w-14 bg-red-300'>red</div>
+<div onClick={()=>setColor("purple")}className='min-h-14 w-14 bg-purple-300'>purple</div>
+
+<div onClick={()=>setColor("red")}className='min-h-14 w-14 bg-red-800'>red</div>
+
 
 </div>
 
