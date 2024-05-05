9004547e2e9acefe421ed1e696724cecc3344bff
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-05 10:11:53 +0545

    bg color

--- bgcolor/src/App.jsx
+++ bgcolor/src/App.jsx
@@ -5,6 +5,15 @@ function App() {
 
   return (
     <>
+    <div className='flex w-full bg-gray-400 min-h-24 items-center justify-between'>
+
+<div className='min-h-14 w-14 bg-green-300'>green</div>
+<div className='min-h-14 w-14 bg-blue-300'>blue</div>
+<div className='min-h-14 w-14 bg-yellow-300'>yellow</div>
+<div className='min-h-14 w-14 bg-red-300'>red</div>
+
+</div>
+
     </>
   )
 }
