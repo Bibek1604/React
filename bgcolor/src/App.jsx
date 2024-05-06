1dfcc7618347554b03221da5e1cd8f88f94c1c52
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-06 10:14:06 +0545

    design and attributes added

--- bgcolor/src/App.jsx
+++ bgcolor/src/App.jsx
@@ -3,27 +3,63 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [color,setColor] =useState('black')
 
 
   return (
-
-
-
-
     <>
-    <div className='flex w-full bg-gray-400 min-h-24 items-center justify-between' style={{backgroundColor:color}}>
+<div className="min-h-screen flex justify-center items-center bg-gray-200">
+  <div className="min-h-32 w-120 bg-slate-700 flex flex-col justify-center items-center">
 
-<div onClick={()=>setColor("green")}  className='min-h-14 w-14 bg-green-300'>green</div>
-<div onClick={()=>setColor("blue")}className='min-h-14 w-14 bg-blue-300'>blue</div>
-<div onClick={()=>setColor("yellow")}className='min-h-14 w-14 bg-yellow-300'>yellow</div>
-<div onClick={()=>setColor("red")}className='min-h-14 w-14 bg-red-300'>red</div>
-<div onClick={()=>setColor("purple")}className='min-h-14 w-14 bg-purple-300'>purple</div>
+    <div className="min-h-18 w-full flex justify-between items-center bg-zinc-700 p-3 rounded">
+      <div className="flex items-center gap-x-3">
+        <span></span>
+        <input
+          type="text"
+          value=" Enter your password here" // add {password} here
+          className="outline-none py-1 px-3 flex-grow"
+          placeholder="password"
+          readOnly
+        />
+      </div>
+      <button
+        className='outline-none bg-blue-700 text-white px-3 py-1 rounded'
+      >
+        Copy
+      </button>
+    </div>
 
-<div onClick={()=>setColor("red")}className='min-h-14 w-14 bg-red-800'>red</div>
+    <div className='flex text-5m gap-x-2 mt-3 items-center'>
+      <div className='flex items-center gap-x-3'>
+        <input
+          type='range'
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+        />
+        <label>length: 8</label>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <input
+          type="checkbox"
+          id="includeCharacter"
+          className="cursor-pointer"
+        />
+        <label to="includeCharacter">Include Characters</label>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <input
+          type="checkbox"
+          id="includeNumber"
+          className="cursor-pointer"
+        />
+        <label to="includeNumber">Include Numbers</label>
+      </div>
+    </div>
 
-
+  </div>
 </div>
+
 
     </>
   )
