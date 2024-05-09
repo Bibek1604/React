ccea09c137df3a5e0c8578f946f630fc15f2f44a
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-09 23:30:36 +0545

    toggle button worked

--- themecontext/src/App.jsx
+++ themecontext/src/App.jsx
@@ -1,7 +1,7 @@
 import  { useEffect, useState } from 'react';
 import './App.css';
-import { ThemeProvider } from './context/theme';
-import ThemeBtn from './components/ThemeButton';
+import { ThemeProvider } from './contexts/theme';
+import ThemeBtn from './components/ThemeBtn';
 import Card from './components/Card';
 
 function App() {
